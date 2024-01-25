f2ca4275aa232593ab057af3100542a8a8e2bdb9
ecomnazar <ecomnazar.me@gmail.com>
2024-01-25 17:46:10 +0500

    [fixed]: any bugs

--- src/Lessons/Day1Theme1.tsx
+++ src/Lessons/Day1Theme1.tsx
@@ -1,13 +1,9 @@
-import 'swiper/css';
-import { Swiper, SwiperRef, SwiperSlide } from "swiper/react"
 import React from "react";
 import clsx from "clsx";
-import { data } from "../pages/Course";
-import { useNavigate } from "react-router-dom";
-import { useLogicStore } from "../stores/useLogicStore";
-import { getThemeLS, getTickLS, setActiveDayLS, setActiveDayThemeLS, setThemeLS, setTickLs } from "../lib/localStorage";
+import { Swiper, SwiperRef, SwiperSlide } from "swiper/react"
 import LessonNavigatorButtons from "../components/LessonNavigatorButtons";
 import i18n from '../i18n';
+import 'swiper/css';
 
 const content = [
     "/lesson-images/day-1/1",
@@ -18,57 +14,6 @@ const Day1Theme1 = () => {
     const swiperRef = React.useRef<SwiperRef>(null)
     const [activeIndex, setActiveIndex] = React.useState(0)
     const currentLanguage = i18n.language
-
-    const navigate = useNavigate()
-    // to set main content in left side
-    const active = useLogicStore((state) => state.active)
-    const setActive = useLogicStore((state) => state.setIsActive)
-    // to set color to text
-    const theme = useLogicStore((state) => state.theme)
-    const setTheme = useLogicStore((state) => state.setTheme)
-    // to set tick
-    const tick = useLogicStore((state) => state.tick)
-    const setTick = useLogicStore((state) => state.setTick)
-
-    // FLS from local storage
-    const themeLS = getThemeLS()
-    const tickLS = getTickLS()
-
-    const onClickNext = () => {
-        if (activeIndex === content.length - 1) {
-            // if works when all days finished
-            if (data.length === active[0] + 1 && data[active[0]].length === active[1] + 1) {
-                navigate('/test-page')
-            } else {
-                setTick([...tick, data[active[0]][active[1]].title])
-
-                // when first open page works if
-                if (!tickLS) {
-                    setTickLs(data[active[0]][active[1]].title + '::')
-                } else {
-                    setTickLs((themeLS + data[active[0]][active[1]].title) + '::')
-                }
-
-                // if works when all lessons of day finished
-                if (data[active[0]].length === active[1] + 1) {
-                    setTheme([...theme, data[active[0] + 1][0].title])
-                    setActive([active[0] + 1, 0])
-                    // LS
-                    setThemeLS(themeLS + data[active[0] + 1][0].title + '::')
-                    setActiveDayLS((active[0] + 1).toString())
-                    setActiveDayThemeLS('0')
-                } else {
-                    setTheme([...theme, data[active[0]][active[1] + 1].title])
-                    setActive([active[0], active[1] + 1])
-                    // LS
-                    themeLS ? setThemeLS(themeLS + data[active[0]][active[1] + 1].title + '::') : setThemeLS(theme[0] + data[active[0]][active[1] + 1].title)
-                    setActiveDayThemeLS((active[1] + 1).toString())
-                }
-            }
-        } else {
-            swiperRef.current?.swiper.slideNext()
-        }
-    }
 
     const onClickPrev = () => {
         swiperRef.current?.swiper.slidePrev()
@@ -99,7 +44,7 @@ const Day1Theme1 = () => {
                     )
                 })}
             </div>
-            <LessonNavigatorButtons activeIndex={activeIndex} onClickNext={onClickNext} onClickPrev={onClickPrev} content={content} />
+            <LessonNavigatorButtons activeIndex={activeIndex} swiperRef={swiperRef} onClickPrev={onClickPrev} content={content} />
         </>
     )
 }
--- src/components/LessonNavigatorButtons.tsx
+++ src/components/LessonNavigatorButtons.tsx
@@ -1,18 +1,60 @@
 import React from 'react'
-import Button from './Button'
 import clsx from 'clsx'
-import { MdKeyboardArrowUp } from 'react-icons/md'
 import { useTranslation } from 'react-i18next'
+import { MdKeyboardArrowUp } from 'react-icons/md'
+import Button from './Button'
+import { useLogicStore } from '../stores/useLogicStore'
+import { getThemeLS, getTickLS, setActiveDayThemeLS, setThemeLS, setTickLs } from '../lib/localStorage'
+import { data } from '../pages/Course'
+import { SwiperRef } from 'swiper/react'
 
 interface Props {
     activeIndex: number
-    onClickNext: () => void
     onClickPrev: () => void
     content: any[];
+    swiperRef: React.RefObject<SwiperRef>;
 }
 
-const LessonNavigatorButtons: React.FC<Props> = ({ activeIndex, onClickNext, onClickPrev, content }) => {
+const LessonNavigatorButtons: React.FC<Props> = ({ activeIndex, onClickPrev, content, swiperRef }) => {
     const { t } = useTranslation()
+
+    // to set main content in left side
+    const active = useLogicStore((state) => state.active)
+    const setActive = useLogicStore((state) => state.setIsActive)
+    // to set color to text
+    const theme = useLogicStore((state) => state.theme)
+    const setTheme = useLogicStore((state) => state.setTheme)
+    // to set tick
+    const tick = useLogicStore((state) => state.tick)
+    const setTick = useLogicStore((state) => state.setTick)
+
+    // FLS from local storage
+    const themeLS = getThemeLS()
+    const tickLS = getTickLS()
+
+    const onClickNext = () => {
+        // check slide end or not
+        if (activeIndex === content.length - 1) {
+            setTick([...tick, data[active[0]][active[1]].title])
+
+            // when first open page works if
+            if (!tickLS) {
+                setTickLs(data[active[0]][active[1]].title + '::')
+            } else {
+                setTickLs((themeLS + data[active[0]][active[1]].title) + '::')
+            }
+
+            setTheme([...theme, data[active[0]][active[1] + 1].title])
+            setActive([active[0], active[1] + 1])
+            // LS
+            themeLS ? setThemeLS(themeLS + data[active[0]][active[1] + 1].title + '::') : setThemeLS(theme[0] + data[active[0]][active[1] + 1].title)
+            setActiveDayThemeLS((active[1] + 1).toString())
+
+        } else {
+            swiperRef.current?.swiper.slideNext()
+        }
+    }
+
     return (
         <div className="flex items-center justify-between w-full">
             <Button
